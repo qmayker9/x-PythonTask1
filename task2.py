0f1444cafaaece2ca384ac24dc71e2173abbae64
qmayker9 <dusttoplegenda@gmail.com>
2024-08-01 14:05:36 +0300

    Теперь если отправить пустой список функция не ломается

--- task2.py
+++ task2.py
@@ -1,7 +1,11 @@
 def fix_ingredients(ingredients: list) -> list:
+    if len(ingredients) == 0:
+        return []
+
     fixed_ingredients = []
 
     for ingredient in ingredients:
+
         if ingredient.lower()[0] != 'z':
 
             fixed_ingredients.append(ingredient[::-1])
